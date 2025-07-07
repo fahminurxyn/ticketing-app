76c6893910766974c2c77c566ee5dccb0ee58e2d
Fahmi Nur Yudisyah <fahminur.sekawanmedia@gmail.com>
2025-07-07 14:10:22 +0700

    Create Ticket model and define relationships

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function approvals(): HasMany
+    {
+        return $this->hasMany(TicketApproval::class);
+    }
+
+    public function histories(): HasMany
+    {
+        return $this->hasMany(TicketHistory::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -22,7 +24,27 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function superior(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'superior_id');
+    }
+
+    public function subordinates(): HasMany
+    {
+        return $this->hasMany(User::class, 'superior_id');
+    }
+
+    public function tickets(): HasMany
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
+    /*
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
