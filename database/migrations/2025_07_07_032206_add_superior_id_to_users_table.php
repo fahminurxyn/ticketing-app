743062567088ca88b8224708291f15ce6cebcac1
Fahmi Nur Yudisyah <fahminur.sekawanmedia@gmail.com>
2025-07-07 12:47:35 +0700

    Make models and migrations database

---
+++ database/migrations/2025_07_07_032206_add_superior_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(User::class, 'superior_id')
+                ->nullable()
+                ->after('role_id')
+                ->constrained('users')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['superior_id']);
+            $table->dropColumn('superior_id');
+        });
+    }
+};
---
+++ database/migrations/2025_07_07_032809_create_tickets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Category::class)->nullable()->constrained()->nullOnDelete();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+            $table->enum('status', ['DRAFT', 'SUBMITTED', 'APPROVED', 'REJECTED', 'IN_PROGRESS', 'CLOSED'])->default('DRAFT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
